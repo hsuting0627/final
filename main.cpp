3a20ba1f40f05fb72ce8edb257527638bd27cbad
hsuting0627 <95949110+hsuting0627@users.noreply.github.com>
2022-06-01 15:21:20 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,219 @@
+    #include "mbed.h"
+    #include "bbcar.h"
+
+    #define velocity 60
+    #define CENTER_BASE 1500
+
+    //BufferedSerial pc(USBTX, USBRX);
+    BufferedSerial serdev(D1, D0, 9600);
+
+    Ticker servo_ticker;
+    Ticker encoder_ticker;
+    Thread qti;
+    Thread Move;
+    Thread Distance;
+    Thread PING;
+
+    PwmOut pin5(D10), pin6(D11);
+    DigitalIn encoder(D3); // encoder
+    DigitalInOut pin10(D9); // ping 
+    volatile int steps;
+    volatile int last;
+    BBCar car(pin5, pin6, servo_ticker);
+
+    // QTI Sensor
+    DigitalInOut pin1(D4);
+    DigitalInOut pin2(D5);
+    DigitalInOut pin3(D6);
+    DigitalInOut pin4(D7);
+    int Sensor1;
+    int Sensor2;
+    int Sensor3;
+    int Sensor4;
+    int US = 20;
+    int wL, wR;
+    // Branch
+    int right_turn = 0;
+    int left_turn = 0;
+
+    // QTI Sensing System in thread
+    void qti_sensing(){
+        while (true) {
+            pin1.output();
+            pin2.output();
+            pin3.output();
+            pin4.output();
+
+            pin1.write(1);
+            pin2.write(1);
+            pin3.write(1);
+            pin4.write(1);
+
+            ThisThread::sleep_for(1ms);
+
+            pin1.input();
+            pin2.input();
+            pin3.input();
+            pin4.input();
+
+            pin1.write(0);
+            pin2.write(0);
+            pin3.write(0);
+            pin4.write(0);
+
+            wait_us(400);
+            Sensor1 = pin1.read();
+            Sensor2 = pin2.read();
+            Sensor3 = pin3.read();
+            Sensor4 = pin4.read();
+ //           printf("%d, %d, %d, %d \n", Sensor1,Sensor2, Sensor3,Sensor4);
+        }
+    }
+    void ping(){
+        parallax_ping  ping1(pin10);
+        while(1){
+        US = (float)ping1;
+        ThisThread::sleep_for(300ms);
+        }
+    }
+    // encoder
+    void encoder_control() {
+        int value = encoder;
+        if (!last && value) steps++;
+        last = value;
+    }
+
+    int get_speed(){
+        float init;
+        float fin;
+        int speed;
+        init = steps * 6.5 * 3.14 / 32;
+        ThisThread::sleep_for(100ms);
+        fin = steps * 6.5 * 3.14 / 32;
+        speed = (fin - init) * 10;
+        return speed;
+    }
+
+    // Servo Control System
+    void servo0_control(int speed) {
+        if (speed > 200)       speed = 200;
+        else if (speed < -200) speed = -200;
+
+        pin5 = (CENTER_BASE + speed)/20000.0f;
+    }
+    void servo1_control(int speed) {
+        if (speed > 200)       speed = 200;
+        else if (speed < -200) speed = -200;
+
+        pin6 = (CENTER_BASE + speed)/20000.0f;
+    }
+
+    // Moving Thread
+    void Moving(){
+        int time = 0;
+        pin5.period_ms(20);
+        pin6.period_ms(20);
+        while (1) {
+            wL = 0;
+            wR = 0;
+            if (US < 25)
+            {
+                wL = 60;
+                wR = 60*0.6;
+                time = 200000;
+            }
+            else if (Sensor1 == 1 && Sensor2 == 0 && Sensor3 == 0 && Sensor4 == 0){
+                wL = velocity * 0.05;
+                wR = -velocity;
+                time = 1;
+            }
+            else if (Sensor1 == 1 && Sensor2 == 1 && Sensor3 == 0 && Sensor4 == 0){
+                wL = velocity * 0.2;
+                wR = -velocity;
+                time = 1;
+            }
+
+            else if (Sensor1 == 1 && Sensor2 == 1 && Sensor3 == 1 && Sensor4 == 0){
+                time = 40000;
+                wL = 100 * 0.1;
+                wR = -100;
+            }
+            else if (Sensor1 == 0 && Sensor2 == 1 && Sensor3 == 1 && Sensor4 == 1){
+                time = 40000;
+                wL = 100;
+                wR = -100 * 0.1;
+            }
+
+            else if (Sensor1 == 0 && Sensor2 == 1 && Sensor3 == 1 && Sensor4 == 0){
+                wL = velocity;
+                wR = -velocity;
+                time = 1;
+                }
+
+            else if (Sensor1 == 0 && Sensor2 == 0 && Sensor3 == 1 && Sensor4 == 1){
+                wL = velocity;
+                wR = -velocity * 0.2;
+                time = 1;
+            } 
+
+            else if (Sensor1 == 0 && Sensor2 == 1 && Sensor3 == 0 && Sensor4 == 0) {
+                wL = velocity * 0.8;
+                wR = -velocity ;
+                time = 1;
+            }
+            else if (Sensor1 == 0 && Sensor2 == 0 && Sensor3 == 1 && Sensor4 == 0) {
+                wL = velocity;
+                wR = -velocity * 0.8;
+                time = 1;
+            }
+
+            else if (Sensor1 == 0 && Sensor2 == 0 && Sensor3 == 0 && Sensor4 == 1){
+                wL = velocity;
+                wR = -velocity * 0.05;
+                time = 1000;
+            }  
+            else if (Sensor1 == 0 && Sensor2 == 0 && Sensor3 == 0 && Sensor4 == 0) {
+                wL = 0;
+                wR = 0;
+                time = 1;
+            }
+            else if (Sensor1 == 1 && Sensor2 == 1 && Sensor3 == 1 && Sensor4 == 1) {
+                    wL = 0;
+                    wR = 0;
+                    time = 1;
+            }
+            for (int j = 1 ; j <= time ; j++) {
+                servo0_control(wL);
+                servo1_control(wR);
+            }
+        }
+    }
+
+    // main() runs in its own thread in the OS
+    int main()
+    {
+        int dist;
+        int speed_test;
+        char instr[32];
+
+        encoder_ticker.attach(&encoder_control, 10ms);
+        qti.start(qti_sensing);
+        Move.start(Moving);
+        PING.start(ping);
+        memset(instr, '\0', 32); //clear buffer
+        while(1){
+            dist = steps * 6.5 * 3.14 / 32 ;
+            sprintf(instr, "Distance traveled : %d cm\n", dist);
+            serdev.write(instr, strlen(instr));
+            ThisThread::sleep_for(1s);
+            memset(instr, '\0', 32);
+
+            speed_test = get_speed();
+            sprintf(instr, "Current speed : %d cm\n", speed_test);
+            serdev.write(instr, strlen(instr));
+            ThisThread::sleep_for(1s);
+            memset(instr, '\0', 32);
+        }
+    }
+
+
